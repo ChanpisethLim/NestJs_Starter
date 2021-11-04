70b3f27f9f333710ecf166bf3fcc2fa4b5635170
Chanpiseth Lim <limchanpiseth@gmail.com>
2021-11-04 20:47:21 +0700

    modify ormconfig

--- ormconfig.ts
+++ ormconfig.ts
@@ -1,20 +1,25 @@
 import { PostgresConnectionOptions } from "typeorm/driver/postgres/PostgresConnectionOptions";
+import * as dotenv from 'dotenv'
+
+dotenv.config({path: 'src/.env'})
 
 const config: PostgresConnectionOptions = {
     type: 'postgres',
     host: 'localhost',
     port: 5432,
-    username: 'myuser',
-    password: 'myuser',
-    database: 'test',
-    entities: [__dirname + '/**/entities/*.entity{.ts,.js}'],
+    username: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
+    // entities: [__dirname + '/**/entities/*.entity{.ts,.js}'],
+    entities: ['dist/**/*.entity{.ts,.js}'],
     synchronize: false,
     migrationsRun: false,
     migrations: [
         'dist/src/migrations/*{.ts,.js}'
     ],
     cli: {
-        migrationsDir: 'src/migrations'
+        migrationsDir: 'src/migrations',
+        // entitiesDir: 'src/**/entities/*.entity{.ts,.js}'
     }
 }
 
