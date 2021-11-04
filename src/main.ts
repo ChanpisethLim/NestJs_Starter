1a00e3858b7048f3753f198d7e865eb350efd964
Chanpiseth Lim <limchanpiseth@gmail.com>
2021-11-04 22:09:48 +0700

    add cors

--- src/main.ts
+++ src/main.ts
@@ -11,6 +11,9 @@ async function bootstrap() {
   app.setGlobalPrefix('api')
   app.useGlobalPipes(new ValidationPipe());
   app.use(cookieParser());
+  app.enableCors({
+    credentials: true
+  })
   await app.listen(3000);
 }
 bootstrap();
